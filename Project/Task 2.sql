83525f9d264b8e9ea3cbdd9bea6d4b76a95fe4cc
Saikat Pal <119132976+saikat912@users.noreply.github.com>
2026-07-16 07:17:57 +0530

    Add files via upload

---
+++ Project/Task 2.sql
@@ -0,0 +1,43 @@
+--
+-- Welcome to Task 2 !!
+--
+-- Advanced RDB and SQL
+--
+-- Server Functions
+
+USE world_peace;
+
+-- aggregate functions
+
+
+
+
+-- TODO --
+
+-- Write an SQL statement to:
+-- find the sum of the column quantity
+-- the function is SUM( )
+-- in the table customer_order_line_item
+
+
+
+
+-- Non-aggregate functions
+
+
+
+
+
+-- TODO --
+
+-- Write an SQL statement to:
+
+-- find the average all the unit_price in the table merchandise_item
+-- (don't forget to divide it by 100)
+-- format it to 2 decimal places and use country code "en_IN" for Indian English
+-- add the Rupee symbol ₹ in front of it, copy and paste it if you'd like
+
+
+
+
+
---
+++ Project/Task 6.sql
@@ -0,0 +1,28 @@
+--
+-- Welcome to Task 6 !!
+--
+-- Advanced RDB and SQL
+--
+-- Common Table Expressions
+
+USE world_peace;
+
+WITH order_line_item_cte (new_name, new_order_id, new_description, order_qty, new_unit_price, line_subtotal ) AS
+
+(
+
+-- TODO -----> Paste the customer order SQL here
+
+)
+
+SELECT * from order_line_item_cte;
+
+-- TODO --
+
+-- write a common table expression
+-- call it customer_cte
+-- select two columns customer_id and customer_name
+-- from the table customer
+-- sort by customer_name
+-- run it buy select *
+
